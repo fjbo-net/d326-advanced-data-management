59269537c546e11f95b413c16f2021a75099fefc
fjbo-net <francisco@fjbo.net>
2025-08-03 16:48:54 -0600

    Added curl check to 'remote-setup.bat'

--- scripts/dev/remote-setup.bat
+++ scripts/dev/remote-setup.bat
@@ -26,37 +26,48 @@ SET "GitBinDirectory=%GitDirectory%\bin"
 SET "Git=%GitBinDirectory%\git.exe"
 
 
+:: Check if curl is available
+ECHO.
+ECHO [1/7] Checking if curl is available...
+curl --version >nul 2>&1
+IF ERRORLEVEL 1 (
+    ECHO ERROR: curl is not available. Cannot continue.
+    EXIT /B 1
+)
+ECHO SUCCESS: curl is available
+
+
 :: Download Visual Studio Code install script
 ECHO.
-ECHO [1/6] Downloading VS Code installer script...
+ECHO [2/7] Downloading VS Code installer script...
 CURL -L -o %VsCodeLocalInstallScript% %VsCodeInstallScriptUrl%
 
 
 :: Download Git for Windows install script
 ECHO.
-ECHO [2/6] Downloading Git installer script...
+ECHO [3/7] Downloading Git installer script...
 CURL -L -o %GitLocalInstallerScript% %GitInstallerScriptUrl%
 
 
 :: Run Visual Studio Code install script
 ECHO.
-ECHO [3/6] Installing VS Code...
+ECHO [4/7] Installing VS Code...
 "%VsCodeLocalInstallScript%"
 
 
 :: Run Git for Windows install script
 ECHO.
-ECHO [4/6] Installing Git...
+ECHO [5/7] Installing Git...
 "%GitLocalInstallerScript%"
 
 
 :: Clone repository
 ECHO.
-ECHO [5/6] Cloning repository...
+ECHO [6/7] Cloning repository...
 "%Git%" clone %RepoGitUrl% %LocalRepo%
 
 
 :: Open repository in Visual Studio Code
 ECHO.
-ECHO [6/6] Opening repository in VS Code...
+ECHO [7/7] Opening repository in VS Code...
 CODE %LocalRepo%

03962a17ad8ba4a0d9c5cf29d9f733175ca14278
fjbo-net <francisco@fjbo.net>
2025-08-03 02:42:16 -0600

    Added 'install-git.bat'

---
+++ scripts/dev/install-git.bat
@@ -0,0 +1,17 @@
+:: © 2025 FJBO
+::
+:: Downloads and installs Git for Windows
+
+@ECHO off
+
+SET UserDownloads=%USERPROFILE%\Downloads
+
+SET GitInstallerFileName=Git-2.50.1-64-bit.exe
+SET GitInstallerUrl=https://github.com/git-for-windows/git/releases/download/v2.50.1.windows.1/%GitInstallerFileName%
+SET GitLocalInstaller=%UserDownloads%\%GitInstallerFileName%
+
+:: Download Git for Windows
+CURL -L -o %GitLocalInstaller% %GitInstallerUrl%
+
+:: Install Git for Windows
+%GitLocalInstaller% /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS

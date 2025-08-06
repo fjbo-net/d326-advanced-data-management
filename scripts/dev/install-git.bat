895975ccdfaf92ee0b544d586802c9a0a4d7b763
fjbo-net <francisco@fjbo.net>
2025-08-06 01:26:49 -0700

    Added clean-up procedures to 'install-git.bat'

--- scripts/dev/install-git.bat
+++ scripts/dev/install-git.bat
@@ -22,3 +22,30 @@ ECHO.
 ECHO [2/2] Installing Git for Windows...
 %GitLocalInstaller% /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS
 ECHO SUCCESS: Git for Windows has been installed successfully
+
+
+:: Success cleanup
+ECHO.
+ECHO DONE: Git for Windows has been installed successfully!
+ECHO.
+
+GOTO :CLEANUP_SUCCESS
+
+
+:CLEANUP_ERROR
+ECHO.
+ECHO ERROR: Git for Windows installation could not be completed!
+ECHO.
+
+
+:CLEANUP_SUCCESS
+:: Clean up temporary files
+IF EXIST "%GitLocalInstaller%" (
+	DEL /Q "%GitLocalInstaller%" >nul 2>&1
+)
+
+IF DEFINED CLEANUP_ERROR (
+	EXIT /B 1
+) ELSE (
+	EXIT /B 0
+)

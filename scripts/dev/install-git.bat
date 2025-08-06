a3ccaee641ef78e19dfde21c6330c56f43f61aad
fjbo-net <francisco@fjbo.net>
2025-08-06 01:21:14 -0700

    Added missing quotes in 'install-git.bat'

--- scripts/dev/install-git.bat
+++ scripts/dev/install-git.bat
@@ -4,11 +4,11 @@
 
 @ECHO off
 
-SET UserDownloads=%USERPROFILE%\Downloads
+SET "UserDownloads=%USERPROFILE%\Downloads"
 
-SET GitInstallerFileName=Git-2.50.1-64-bit.exe
-SET GitInstallerUrl=https://github.com/git-for-windows/git/releases/download/v2.50.1.windows.1/%GitInstallerFileName%
-SET GitLocalInstaller=%UserDownloads%\%GitInstallerFileName%
+SET "GitInstallerFileName=Git-2.50.1-64-bit.exe"
+SET "GitInstallerUrl=https://github.com/git-for-windows/git/releases/download/v2.50.1.windows.1/%GitInstallerFileName%"
+SET "GitLocalInstaller=%UserDownloads%\%GitInstallerFileName%"
 
 :: Download Git for Windows
 CURL -L -o %GitLocalInstaller% %GitInstallerUrl%

fd4953fd58432bff2dcce4f62d76ad3349deceec
fjbo-net <francisco@fjbo.net>
2025-08-05 22:48:39 -0700

    Added missing quotes in 'install-vscode.bat'

--- scripts/dev/install-vscode.bat
+++ scripts/dev/install-vscode.bat
@@ -4,12 +4,12 @@
 
 @ECHO off
 
-SET UserDownloads=%USERPROFILE%\Downloads
+SET "UserDownloads=%USERPROFILE%\Downloads"
 
-SET VsCodeInstallerCommit=488a1f239235055e34e673291fb8d8c810886f81
-SET VsCodeInstallerFileName=VSCodeUserSetup-x64-1.102.3.exe
-SET VsCodeInstallerUrl=https://vscode.download.prss.microsoft.com/dbazure/download/stable/%VsCodeInstallerCommit%/%VsCodeInstallerFileName%
-SET VsCodeLocalInstaller=%UserDownloads%\%VsCodeInstallerFileName%
+SET "VsCodeInstallerCommit=488a1f239235055e34e673291fb8d8c810886f81"
+SET "VsCodeInstallerFileName=VSCodeUserSetup-x64-1.102.3.exe"
+SET "VsCodeInstallerUrl=https://vscode.download.prss.microsoft.com/dbazure/download/stable/%VsCodeInstallerCommit%/%VsCodeInstallerFileName%"
+SET "VsCodeLocalInstaller=%UserDownloads%\%VsCodeInstallerFileName%"
 
 :: Download Visual Studio Code installer
 CURL -L -o %VsCodeLocalInstaller% %VsCodeInstallerUrl%

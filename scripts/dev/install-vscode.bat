30e585b95d75525e21fb6a7259768a98b9cc3741
fjbo-net <francisco@fjbo.net>
2025-08-06 00:32:34 -0700

    Added verbose messages to 'install-vscode.bat'

--- scripts/dev/install-vscode.bat
+++ scripts/dev/install-vscode.bat
@@ -12,7 +12,14 @@ SET "VsCodeInstallerUrl=https://vscode.download.prss.microsoft.com/dbazure/downl
 SET "VsCodeLocalInstaller=%UserDownloads%\%VsCodeInstallerFileName%"
 
 :: Download Visual Studio Code installer
+ECHO.
+ECHO [1/2] Downloading Visual Studio Code installer...
 CURL -L -o %VsCodeLocalInstaller% %VsCodeInstallerUrl%
+ECHO SUCCESS: Visual Studio Code installer has been downloaded
+ECHO Installer Location: '%VsCodeLocalInstaller%'
 
 :: Install Visual Studio Code
+ECHO.
+ECHO [2/2] Installing Visual Studio Code...
 %VsCodeLocalInstaller% /VERYSILENT /SUPPRESSMSGBOXES /MERGETASKS="!runcode,addcontextmenufiles,addcontextmenufolders"
+ECHO SUCCESS: Visual Studio has been installed successfully
